f6ad5e95c4d69eb29518269a3136bde18e431739
Exr0n <spotyie@gmail.com>
2020-10-26 17:08:01 -0700

    final script

--- animate.py
+++ animate.py
@@ -2,11 +2,11 @@ import pyautogui as pag
 from subprocess import run
 from time import sleep
 
-center = (pag.size()[0]/2, pag.size()[1]/2)
-hscroll_mult = 38.2*2
-scroll_mult = 21.85*2
 pos = {
-    'repeating_baby': (245, 180)
+    'repeating_baby': (961, 341),
+    'rivets':         (566, 693),
+    'boiler':         (977, 700),
+    'forest':         (1414, 728)
     }
 
 def applescript_click(menu, item):
@@ -23,34 +23,33 @@ set topmost to true
 end tell
 end tell'''])
 
-def zoom_in():
-    applescript_click('View', 'Zoom In')
-
-def zoom_out():
-    applescript_click('View', 'Zoom Out')
+# def zoom_in():
+#     applescript_click('View', 'Zoom In')
+#
+# def zoom_out():
+#     applescript_click('View', 'Zoom Out')
 
 def reset_view():
     pag.press('esc')
     applescript_click('View', 'Presentation Mode')
     pag.moveTo(list(map(lambda x: x-5, pag.size())))
 
-def scroll(x, y):
-    print("uh, scrolling by ", x/100*hscroll_mult, y/100*scroll_mult)
-    pag.hscroll(x/100*hscroll_mult)
-    pag.scroll(y/100*scroll_mult)
+# def scroll(x, y):
+#     print("uh, scrolling by ", x/100*hscroll_mult, y/100*scroll_mult)
+#     pag.hscroll(x/100*hscroll_mult)
+#     pag.scroll(y/100*scroll_mult)
 
 def main():
-    zoom_in()
-    sleep(1)
-    zoom_out()
+    for visual in pos:
+        print('going to ', visual)
+        pag.moveTo(pos[visual])
+        input('press for next..')
 
 if __name__ == '__main__':
     focus_illustrator()
     reset_view()
-    scroll(50, 50)
-    for i in range(10):
-        zoom_in()
-    input()
+    main()
+    pag.moveTo(list(map(lambda x: x/2, pag.size())))
     pag.click()
     pag.press('esc')
 
